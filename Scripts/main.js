9c1a6492887a2d8640eee8fd83175215367cf5ee
Wuattt <asadar34@gmail.com>
2025-09-29 10:29:36 +0300

    refactor: cruiser movement in separate folder

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,36 +1,5 @@
 'use strict'
 
-
-
-function strafeLeft () {
-    dragon.x += 1 * (Math.round(((Math.cos(inRad(dragon.deg) - Math.PI + Number.EPSILON) * 1000))) / 1000);
-    dragon.y += 1 * (Math.round(((Math.sin(inRad(dragon.deg) - Math.PI + Number.EPSILON) * 1000))) / 1000);
-}
-
-function strafeRight () {
-    dragon.x -= 1 * (Math.round(((Math.cos(inRad(dragon.deg) - Math.PI + Number.EPSILON) * 1000))) / 1000);
-    dragon.y -= 1 * (Math.round(((Math.sin(inRad(dragon.deg) - Math.PI + Number.EPSILON) * 1000))) / 1000);
-}
-
-function moveForward () {
-    dragon.x += 1 * (Math.round(((Math.cos(inRad(dragon.deg) - Math.PI / 2 + Number.EPSILON) * 1000))) / 1000);
-    dragon.y += 1 * (Math.round(((Math.sin(inRad(dragon.deg) - Math.PI / 2 + Number.EPSILON) * 1000))) / 1000);
-}
-
-function moveBackwards () {
-    dragon.x -= 1 * (Math.round(((Math.cos(inRad(dragon.deg) - Math.PI / 2 + Number.EPSILON) * 1000))) / 1000);
-    dragon.y -= 1 * (Math.round(((Math.sin(inRad(dragon.deg) - Math.PI / 2 + Number.EPSILON) * 1000))) / 1000);
-}
-
-function rotateLeft () {
-    dragon.deg -= 1;
-}
-
-function rotateRight () {
-    dragon.deg +=1;
-}
-
-
 const setGameSpeed = (coefficient = gameSpeed) => {
     gameSpeed = coefficient;
     clearInterval(timeLoop);
