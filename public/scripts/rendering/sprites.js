36b56f4ddb0440d35a88e66f4e0923ea1bb62262
Wuattt <asadar34@gmail.com>
2026-08-21 14:54:14 +0300

    feat: connect laser sprite

--- public/scripts/rendering/sprites.js
+++ public/scripts/rendering/sprites.js
@@ -1,13 +1,14 @@
 export const SPRITES = {
     cruiser: loadSprite('/Images/Sprites/battlecruiser.png'),
+    cruiser: loadSprite('/Images/Sprites/laser-sprites/33.png'),
+}
+export const ENTITY_VISUALS = {
+    cruiser: {width: 81, height: 118},
+    laser: {width: 80, height: 80}
 }
 
 function loadSprite(src) {
     const img = new Image();
     img.src = src;
     return img;
-}
-
-export const ENTITY_VISUALS = {
-    cruiser: {width: 81, height: 118}
 }
